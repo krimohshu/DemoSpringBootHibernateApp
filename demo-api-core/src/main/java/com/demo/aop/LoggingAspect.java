948cc9b08b86b984378c2e493c8d46c5f5cbe43e
Krishan Shukla <krishan.shyam@gmail.com>
2017-04-26 08:39:52 +0100

    Following concept has been implemented
    REST MVC
    SpringBoot
    AOP 
    Modular approach of Web nd API layer.
    
    Currently Junit and Angular stuff is need to resolve ( they are in this
    build but not working) else this Demo is good to go.

---
+++ demo-api-core/src/main/java/com/demo/aop/LoggingAspect.java
@@ -0,0 +1,39 @@
+package com.demo.aop;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	// @Around("com.demo.controller.HomeController.home(..)")
+	@Around("execution(* com.demo..*.*(..))")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("+05:30"));
+
+		StopWatch sw = new StopWatch();
+		sw.start();
+		Logger logger = LoggerFactory.getLogger(this.getClass());
+		Object result = pjp.proceed();
+		sw.stop();
+		// logger.info("Time to execute the method " +
+		// pjp.getSignature().getName() + " is :" + sw.getTotalTimeMillis());
+
+		logger.info(new StringBuilder("#Timestamp: ").append(cal.getTime()).append(" #Class: ")
+				.append(pjp.getTarget().getClass()).append(" #Method: ").append(pjp.getSignature().getName())
+				.append(" #Execution Time: ").append(sw.getTotalTimeMillis()).append(" ms").toString());
+
+		return result;
+	}
+
+}
